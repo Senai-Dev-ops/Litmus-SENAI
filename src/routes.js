8b3ea8e49c220c80bba00c1735316a3d5af5513a
CauaRodrigues <caua.rodriguesassis205@gmail>
2022-03-18 16:13:49 -0300

    preparação das rotas

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,22 @@
-import React from 'react'
-
-
+import React from "react";
+import { Route, BrowserRouter, Routes, Navigate } from "react-router-dom";
+import Login from "./pages/Login";
+import Dashboard from "./pages/Dashboard";
+import Admin from "./pages/Admin";
 
 export default function Router() {
   return (
-    <div>Router</div>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/*" element={<Navigate to="/" />} />
+
+        <Route path="/Dashboard" element={<Dashboard />} />
+        <Route path="/Dashboard/*" element={<Navigate to="/Dashboard/*" />} />
+
+        <Route path="/Admin" element={<Admin />} />
+        <Route path="/Admin/*" element={<Navigate to="/Admin/*" />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
