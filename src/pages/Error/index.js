230c88ab7df98e195728999603b9163a6287fcac
CauaRodrigues <caua.rodriguesassis205@gmail.com>
2022-03-22 17:43:43 -0300

    Página de Erro

--- src/pages/Error/index.js
+++ src/pages/Error/index.js
@@ -1,7 +1,27 @@
 import React from "react";
+import ErrorImage from "../../assets/error.png";
+import "./style.css";
 
-const Error = ({ code }) => {
-  return <div>Error {code}</div>;
+const Error = ({ error, typeError, text }) => {
+  return (
+    <main className="container-main">
+      <header></header>
+
+      <article className="content">
+        <div className="text-error">
+          {typeError == "code" ? (
+            <h3 className="error code">{error}</h3>
+          ) : (
+            <h3 className="error text">{error}</h3>
+          )}
+
+          <p>{text}</p>
+        </div>
+
+        <img className="image-error" src={ErrorImage} alt="Imagem de erro" />
+      </article>
+    </main>
+  );
 };
 
 export default Error;
--- src/routes.js
+++ src/routes.js
@@ -10,13 +10,30 @@ export default function Router() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login />} />
-        <Route path="/*" element={<Error code="404" />} />
+        <Route
+          path="/*"
+          element={
+            <Error
+              typeError="code"
+              error="404"
+              text="A página que você está procurando não existe."
+            />
+          }
+        />
 
         <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/dashboard/*" element={<Error code="404" />} />
+        <Route
+          path="/dashboard/*"
+          element={
+            <Error
+              error="Acesso restrito."
+              text="Você não tem permissão para acessar esta página. "
+            />
+          }
+        />
 
         <Route path="/admin" element={<Admin />} />
-        <Route path="/admin/*" element={<Error code="404" />} />
+        <Route path="/admin/*" element={<Error error="404" />} />
       </Routes>
     </BrowserRouter>
   );
