d99421f4b696f74af2e9ba28e413ed0c8ab6bdff
CauaRodrigues <caua.rodriguesassis205@gmail.com>
2022-03-22 20:19:52 -0300

    shape divider na página erro

--- src/pages/Error/index.js
+++ src/pages/Error/index.js
@@ -3,25 +3,39 @@ import ErrorImage from "../../assets/error.png";
 import "./style.css";
 
 const Error = ({ error, typeError, text }) => {
-  return (
-    <main className="container-main">
-      <header></header>
+	return (
+		<main className="container-main">
+			<header></header>
 
-      <article className="content">
-        <div className="text-error">
-          {typeError == "code" ? (
-            <h3 className="error code">{error}</h3>
-          ) : (
-            <h3 className="error text">{error}</h3>
-          )}
+			<article className="content">
+				<div className="text-error">
+					{typeError === "code" ? (
+						<h3 className="error code">{error}</h3>
+					) : (
+						<h3 className="error text">{error}</h3>
+					)}
 
-          <p>{text}</p>
-        </div>
+					<p>{text}</p>
+				</div>
 
-        <img className="image-error" src={ErrorImage} alt="Imagem de erro" />
-      </article>
-    </main>
-  );
+				<img className="image-error" src={ErrorImage} alt="Imagem de erro" />
+			</article>
+
+			<div className="custom-shape-divider-bottom-1647989821">
+				<svg
+					data-name="Layer 1"
+					xmlns="http://www.w3.org/2000/svg"
+					viewBox="0 0 1200 120"
+					preserveAspectRatio="none"
+				>
+					<path
+						d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z"
+						class="shape-fill"
+					></path>
+				</svg>
+			</div>
+		</main>
+	);
 };
 
 export default Error;
