dd04ec86623b946ab212cc6c4d6cc4587744c06f
GuilhermeSeveriano <guiseveriano.10@gmail.com>
2022-03-25 17:30:45 -0300

    Header Ok

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,8 +1,25 @@
+import React from "react";
 import "./style.css";
 import LgSenai from "../../assets/images/senai.png";
 import Dscr from "../../assets/images/descri.png";
+import Button from "@material-ui/core/Button";
+import Menu from "@material-ui/core/Menu";
+import MenuItem from "@material-ui/core/MenuItem";
+import ExitToAppIcon from '@material-ui/icons/ExitToApp';
+import SettingsIcon from '@material-ui/icons/Settings';
 
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 const Navbar = () => {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
     <>
       <div className="container-space">
@@ -15,7 +32,26 @@ const Navbar = () => {
           <div className="page">DASHBOARD</div>
 
           <div className="user">
-            <div className="username">Guilherme</div>
+            <div className="username">
+              userName
+              <Button
+                aria-controls="simple-menu"
+                aria-haspopup="true"
+                onClick={handleClick}
+              >
+                <ExpandMoreIcon color="primary" />
+              </Button>
+              <Menu
+                id="simple-menu"
+                anchorEl={anchorEl}
+                keepMounted
+                open={Boolean(anchorEl)}
+                onClose={handleClose}
+              >
+                <MenuItem onClick={handleClose}><SettingsIcon/> Gerenciar Usuários</MenuItem>
+                <MenuItem onClick={handleClose}><ExitToAppIcon/> Logout</MenuItem>
+              </Menu>
+            </div>
           </div>
         </div>
       </div>
