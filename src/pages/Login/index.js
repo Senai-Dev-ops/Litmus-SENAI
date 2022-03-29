7f9982e59f78118b322a2c2103754f50188a0716
CauaRodrigues <caua.rodriguesassis205@gmail.com>
2022-03-29 17:21:02 -0300

    página de login | Ok

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,87 +1,123 @@
-import React from "react";
+import React, { useState } from "react";
 import EmailOutlinedIcon from "@mui/icons-material/EmailOutlined";
 import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 import LogoSenai from "../../assets/svgs/LogoSenai.svg";
 import "./style.css";
-import { purple, red } from "@mui/material/colors";
 
 const Login = () => {
-  const focusField = (i) => {
-    document.getElementsByClassName("field")[i].style.borderColor = "#000";
+  const [colorIconEmail, setColorIconEmail] = useState("var(--gray-secondary)");
+  const [colorIconLock, setColorIconLock] = useState("var(--gray-secondary)");
+
+  const changeIconColor = (focus, icon) => {
+    if (icon === "email") {
+      setColorIconEmail(() =>
+        focus ? "var(--primary)" : "var(--gray-secondary)"
+      );
+    } else if (icon === "lock") {
+      setColorIconLock(() =>
+        focus ? "var(--primary)" : "var(--gray-secondary)"
+      );
+    }
   };
 
-  const focusOut = (i) => {
-    document.getElementsByClassName("field")[i].style.borderColor =
-      "var(--gray-secondary)";
-  };
+  const [borderColorFieldEmail, setBorderColorFieldEmail] = useState(
+    "var(--gray-secondary)"
+  );
+  const [borderColorFieldPassword, setBorderColorFieldPassword] = useState(
+    "var(--gray-secondary)"
+  );
 
-  // const fieldStyles = {
-  //   borderColor: fieldFocus ? red : purple,
-  // };
+  const changeBorderColor = (focus, field) => {
+    if (field === "email") {
+      setBorderColorFieldEmail(() =>
+        focus ? "var(--primary)" : "var(--gray-secondary)"
+      );
+    } else if (field === "password") {
+      setBorderColorFieldPassword(() =>
+        focus ? "var(--primary)" : "var(--gray-secondary)"
+      );
+    }
+  };
 
   return (
     <main className="login">
       <section className="bg-image"></section>
 
       <section className="container-form">
-        <div className="content">
-          <img
-            src={LogoSenai}
-            alt="Logo Senai Vermelho"
-            className="logo-senai"
-          />
-
-          <form>
-            <div className="title">
-              <h1>Fazer Login</h1>
+        <div className="box-content">
+          <div className="content">
+            <div className="box-logo">
+              <img
+                src={LogoSenai}
+                alt="Logo Senai Vermelho"
+                className="logo-senai"
+              />
             </div>
 
-            <div className="fields">
-              <div className="field">
-                <EmailOutlinedIcon className="icon-field" />
-                <input
-                  type="email"
-                  placeholder="Email"
-                  required
-                  onFocus={() => {
-                    const a = (document.getElementsByClassName(
-                      "field"
-                    )[0].style.borderColor = "#000");
-                  }}
-                  onBlur={() => {
-                    const a = (document.getElementsByClassName(
-                      "field"
-                    )[0].style.borderColor = "var(--gray-secondary)");
-                  }}
-                />
+            <form>
+              <div className="title">
+                <h1>Fazer login</h1>
               </div>
 
-              <div className="field">
-                <LockOutlinedIcon className="icon-field" />
-                <input
-                  type="password"
-                  placeholder="Senha"
-                  required
-                  onFocus={() => {
-                    const a = (document.getElementsByClassName(
-                      "field"
-                    )[1].style.borderColor = "#000");
-                  }}
-                  onBlur={() => {
-                    const a = (document.getElementsByClassName(
-                      "field"
-                    )[1].style.borderColor = "var(--gray-secondary)");
-                  }}
-                />
+              <div className="fields">
+                <div
+                  className="field"
+                  style={{ borderColor: borderColorFieldEmail }}
+                >
+                  <EmailOutlinedIcon
+                    className="icon-field"
+                    style={{ color: colorIconEmail }}
+                  />
+                  <input
+                    type="email"
+                    placeholder="Email"
+                    required
+                    onFocus={() => {
+                      changeBorderColor(true, "email");
+                      changeIconColor(true, "email");
+                    }}
+                    onBlur={() => {
+                      changeBorderColor(false, "email");
+                      changeIconColor(false, "email");
+                    }}
+                  />
+                </div>
+
+                <div
+                  className="field"
+                  style={{ borderColor: borderColorFieldPassword }}
+                >
+                  <LockOutlinedIcon
+                    className="icon-field"
+                    style={{ color: colorIconLock }}
+                  />
+                  <input
+                    type="password"
+                    placeholder="Senha"
+                    required
+                    onFocus={() => {
+                      changeBorderColor(true, "password");
+                      changeIconColor(true, "lock");
+                    }}
+                    onBlur={() => {
+                      changeBorderColor(false, "password");
+                      changeIconColor(false, "lock");
+                    }}
+                  />
+                </div>
               </div>
-            </div>
 
-            <div className="link">
-              <p>Esqueci minha senha</p>
-            </div>
+              <div className="box-link">
+                <button type="button" className="false-link">
+                  Esqueci minha senha
+                </button>
+              </div>
 
-            <button type="submit">Entrar</button>
-          </form>
+              <button type="submit" className="btn">
+                Entrar
+              </button>
+            </form>
+          </div>
         </div>
       </section>
     </main>
