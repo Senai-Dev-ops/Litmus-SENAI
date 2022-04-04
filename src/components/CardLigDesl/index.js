0bf9679f9badc56d99f3bf3d099878edea490e83
CauaRodrigues <caua.rodriguesassis205@gmail.com>
2022-04-04 14:37:55 -0300

    correções

--- src/components/CardLigDesl/index.js
+++ src/components/CardLigDesl/index.js
@@ -4,11 +4,11 @@ import "./style.css";
 const CardLigDesl = () => {
   return (
     <div className="container-lig-off">
-        <h3 className="content-lig-off">Desligado / Ligado</h3>
-        <label className="switch-lig-off">
-          <input/>
-          <span className="slider-lig-off" />
-        </label>
+      <h3 className="content-lig-off">Desligado / Ligado</h3>
+      <label className="switch-lig-off">
+        <input type="checkbox" />
+        <span className="slider-lig-off" />
+      </label>
     </div>
   );
 };
