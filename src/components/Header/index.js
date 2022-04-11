fa0c1c748408fbd10b58649fdbe0ebfd409537a4
CauaRodrigues <caua.rodriguesassis205@gmail.com>
2022-04-11 14:32:45 -0300

    header responsiva

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -13,87 +13,94 @@ import MenuIcon from "@mui/icons-material/Menu";
 import "./style.css";
 
 const Header = ({ titleHeader }) => {
-	const username = "James Ferreira";
-	const Navigate = useNavigate();
-	const [anchorEl, setAnchorEl] = React.useState(null);
+  const username = "James Ferreira";
+  const Navigate = useNavigate();
+  const [anchorEl, setAnchorEl] = React.useState(null);
 
-	const handleClick = (event) => {
-		setAnchorEl(event.currentTarget);
-	};
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
 
-	const handleClose = () => {
-		setAnchorEl(null);
-	};
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
 
-	return (
-		<>
-			<div className="container-space">
-				<div className="top">
-					<div className="logo">
-						<img className="lg-senai" src={LgSenai} alt="logo senai" />
-						<img className="descript-logo" src={Dscr} alt="descrição senai" />
-					</div>
+  return (
+    <>
+      <div className="container-space">
+        <div className="top">
+          <div className="logo">
+            <img
+              className="lg-senai"
+              src={LgSenai}
+              alt="logo senai"
+              onClick={() => {
+                Navigate("/dashboard");
+              }}
+            />
+            <img className="descript-logo" src={Dscr} alt="descrição senai" />
+          </div>
 
-					<h1 className="page">{titleHeader}</h1>
+          <h1 className="page">{titleHeader}</h1>
 
-					<div className="menu">
-						<div className="options-menu">
-							<h4 className="username" onClick={handleClick}>
-								{username}
-							</h4>
-							<IconButton onClick={handleClick}>
-								<ExpandMoreIcon color="action" />
-							</IconButton>
-							<Menu
-								id="simple-menu"
-								anchorEl={anchorEl}
-								keepMounted
-								open={Boolean(anchorEl)}
-								onClose={handleClose}
-							>
-								<MenuItem
-									onClick={() => {
-										Navigate("/admin");
-									}}
-								>
-									<div className="items-menu-header">
-										<SettingsIcon style={{ marginRight: "8px" }} />
-										<p>Gerenciar Usuários</p>
-									</div>
-								</MenuItem>
-								<MenuItem
-									onClick={() => {
-										Navigate("/dashboard");
-									}}
-								>
-									<div className="items-menu-header">
-										<InsightsIcon style={{ marginRight: "8px" }} />
-										<p>Dashboard</p>
-									</div>
-								</MenuItem>
-								<MenuItem
-									onClick={() => {
-										Navigate("/");
-									}}
-								>
-									<div className="items-menu-header">
-										<ExitToAppIcon style={{ marginRight: "8px" }} />
-										<p>Logout</p>
-									</div>
-								</MenuItem>
-							</Menu>
-						</div>
-					</div>
+          <div className="menu">
+            <div className="options-menu">
+              <h4 className="username" onClick={handleClick}>
+                {username}
+              </h4>
+              <IconButton onClick={handleClick}>
+                <ExpandMoreIcon color="action" />
+              </IconButton>
+              <Menu
+                id="simple-menu"
+                anchorEl={anchorEl}
+                keepMounted
+                open={Boolean(anchorEl)}
+                onClose={handleClose}
+              >
+                <MenuItem
+                  onClick={() => {
+                    Navigate("/admin");
+                  }}
+                >
+                  <div className="items-menu-header">
+                    <SettingsIcon style={{ marginRight: "8px" }} />
+                    <p>Gerenciar Usuários</p>
+                  </div>
+                </MenuItem>
+                <MenuItem
+                  onClick={() => {
+                    Navigate("/dashboard");
+                  }}
+                >
+                  <div className="items-menu-header">
+                    <InsightsIcon style={{ marginRight: "8px" }} />
+                    <p>Dashboard</p>
+                  </div>
+                </MenuItem>
+                <MenuItem
+                  onClick={() => {
+                    Navigate("/");
+                  }}
+                >
+                  <div className="items-menu-header">
+                    <ExitToAppIcon style={{ marginRight: "8px" }} />
+                    <p>Logout</p>
+                  </div>
+                </MenuItem>
+              </Menu>
+            </div>
+          </div>
 
-					<div className="menu-mobile">
-						<IconButton>
-							<MenuIcon sx={{ fontSize: 40 }} />
-						</IconButton>
-					</div>
-				</div>
-			</div>
-		</>
-	);
+          <div className="menu-mobile">
+            <IconButton>
+              <MenuIcon sx={{ fontSize: 40 }} />
+            </IconButton>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Header;
