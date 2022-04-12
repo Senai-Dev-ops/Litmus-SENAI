807547abe2210d80122212e1c21cec81c8831b4a
Vitor Benedito <vitor.bsdc@gmail.com>
2022-04-12 20:32:13 -0300

    New className's no componente

--- src/components/CardTimeExecution/index.js
+++ src/components/CardTimeExecution/index.js
@@ -4,9 +4,9 @@ import "./style.css";
 const CardTimeExecution = () => {
     return(
         <div className="container-execution">
-            <h3 className="content-execution">Tempo de execução</h3>
+            <h3 className="content-execution h3-execution">Tempo de execução</h3>
             <div className="counter-execution">
-                <h1 className="number-execution">71</h1><h2 className="number-execution">seg</h2>
+                <h1 className="number-execution h1-execution">71</h1><h2 className="number-execution h2-execution">seg</h2>
             </div>
         </div>
     );
