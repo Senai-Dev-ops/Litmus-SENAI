4439a19cfb63061f443ccda846266903ceebd090
CauaRodrigues <caua.rodriguesassis205@gmail.com>
2022-05-06 17:12:25 -0300

    Card de hora atual

--- src/components/Clock/index.js
+++ src/components/Clock/index.js
@@ -1,13 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./style.css";
 
 const Clock = () => {
+  const [displayDate1, setDisplayDate1] = useState("00:00:00");
+  function currentTime() {
+    var date = new Date(),
+      displayDate;
+    if (navigator.userAgent.toLowerCase().indexOf("firefox") > -1) {
+      displayDate = date.toLocaleTimeString("pt-BR");
+    } else {
+      displayDate = date.toLocaleTimeString("pt-BR", {
+        timeZone: "America/Sao_Paulo",
+      });
+    }
+    setDisplayDate1(displayDate);
+  }
+
+  setInterval(currentTime, 1000);
+
   return (
     <div className="box-clock">
       <span className="title-clock">Data / Hora</span>
 
       <div className="content-clock">
-        <span class="clock">13:38</span>
+        <span class="clock">{displayDate1}</span>
 
         <span className="date">02/02/2022</span>
       </div>
