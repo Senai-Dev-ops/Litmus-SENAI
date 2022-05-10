bbbd4fb39d599405adb671cafd92165f2f875cbe
CauaRodrigues <caua.rodriguesassis205@gmail.com>
2022-05-09 22:13:58 -0300

    primeira parte da estrutura

--- src/pages/Dashboard/Teste.js
+++ src/pages/Dashboard/Teste.js
@@ -2,126 +2,50 @@ import React from "react";
 import Header from "../../components/Header";
 import CardLigDesl from "../../components/CardLigDesl";
 import CardTimeExecution from "../../components/CardTimeExecution";
-import ChartRpm from "../../components/ChartRpm";
 import Alarme from "../../components/Alarme";
 import Clock from "../../components/Clock";
-import { Grid } from "@mui/material";
-import { styled } from "@mui/material/styles";
-import { Paper } from "@material-ui/core";
 import ChartFeedRate from "../../components/ChartFeedRate";
 import ChartTemperature from "../../components/ChartTemperature";
 import ChartArea from "../../components/ChartArea";
-
 import "./style.css";
 
-const Item = styled(Paper)(({ theme }) => ({
-  backgroundColor: "#F4F4F4",
-  padding: theme.spacing(1),
-  borderRadius: 10,
-  filter: "drop-shadow(0px 1px 6px rgba(0, 0, 0, 0.25))",
-}));
-
 const Dashboard = () => {
-  return (
-    <>
-      <Header titleHeader="Dashboard" />
+	return (
+		<>
+			<Header titleHeader="Dashboard" />
 
-      <main style={{ padding: 20 }}>
-        <div className="container-cards-dash">
-          <div className="box-card">
-            <CardLigDesl />
-          </div>
+			<main style={{ padding: 20 }}>
+				<div className="container-cards-dash">
+					<div className="box-card" style={{ width: "20%" }}>
+						<CardLigDesl />
+					</div>
 
-          <div className="box-card">
-            <CardTimeExecution />
-          </div>
+					<div className="box-card" style={{ width: "22%" }}>
+						<CardTimeExecution />
+					</div>
 
-          <div className="box-card">
-            <Alarme />
-          </div>
-        </div>
+					<div className="box-card" style={{ width: "26%" }}>
+						<Alarme />
+					</div>
 
-        <div></div>
+					<div className="box-card" style={{ flex: 1 }}>
+						<Clock />
+					</div>
+				</div>
 
-        <div></div>
+				<div className="box-card">
+					<ChartFeedRate />
+				</div>
 
-        <div>
-          <Clock />
-        </div>
+				<div className="box-card">
+					<ChartTemperature />
+				</div>
 
-        <div>
-          <ChartFeedRate />
-        </div>
-
-        <div>
-          <ChartTemperature />
-        </div>
-
-        <div>
-          <ChartArea />
-        </div>
-
-        {/* <Grid container spacing={4} rowSpacing={4}>
-          <Grid
-            container
-            item
-            justifyContent={"center"}
-            spacing={4}
-            rowSpacing={4}
-          >
-            <Grid item xs="auto">
-              <Item>
-                <CardLigDesl />
-              </Item>
-            </Grid>
-
-            <Grid item xs="auto">
-              <Item>
-                <CardTimeExecution />
-              </Item>
-            </Grid>
-
-            <Grid item xs="auto" md={4}>
-              <Item>
-                <Alarme />
-              </Item>
-            </Grid>
-
-            <Grid item xs="auto" md={4}>
-              <Item>
-                <Clock />
-              </Item>
-            </Grid>
-          </Grid>
-
-          <Grid
-            container
-            item
-            spacing={4}
-            justifyContent="center"
-            rowSpacing={4}
-          >
-            <Grid item xs="auto" md={4}>
-              <Item>
-                <ChartFeedRate />
-              </Item>
-            </Grid>
-
-            <Grid item xs="auto" md={4}>
-              <Item>
-                <ChartTemperature />
-              </Item>
-            </Grid>
-
-            <Grid item xs="auto" md={4}>
-              <Item>
-                <ChartArea />
-              </Item>
-            </Grid>
-          </Grid>
-        </Grid> */}
-      </main>
-    </>
-  );
+				<div className="box-card">
+					<ChartArea />
+				</div>
+			</main>
+		</>
+	);
 };
 export default Dashboard;
